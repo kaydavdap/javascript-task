2f94fd27fc3e42cb0e64c8e8881cf4c6d0841822
Adegbola David <kayex2010@yahoo.com>
2020-03-27 23:14:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var name= "David Adegbola";
+var courses=["html", "css", "js", "git"];
+console.log("Hello friends, my name is " + name);
+console.log("my courses are " + courses);
+for(var i=1;i<=200;i++) {if(i%2===0){
+	console.log(i);
+}
+}
+
+	// The 1st two codes, that is var name and var course are place holders holding the name and my courses respectively. 
+//The console.log displays my name and my courses. 
